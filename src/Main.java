d41dce1ab2f7f4c8bc19eadef6b4790940a528f3
Leonardo Abrantes <leonardoabrantes1127@gmail.com>
2025-03-19 11:25:37 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,46 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/*
+VAR
+queijo,presunto,carne,sanduba <- real
+
+INICIO
+ler sanduba
+queijo <- (sanduba * 50)/1000
+presunto <- (sanduba * 50)/1000
+carne <- (sanduba * 100)/1000
+
+escreva "blahblahblah"
+
+FIM
+
+TESTE DE MESA
+
+| Sanduíches | Queijo (sanduba * 50 / 1000) | Presunto (sanduba * 50 / 1000) | Carne (sanduba * 100 / 1000) | Saída |
+|------------|----------------------------|------------------------------|----------------------------|---------------------------------------------------------------|
+| 1          | 0.05                        | 0.05                         | 0.10                        | "Será necessário comprar 0.05Kg de queijo, 0.05Kg de presunto e 0.10Kg de carne." |
+| 5          | 0.25                        | 0.25                         | 0.50                        | "Será necessário comprar 0.25Kg de queijo, 0.25Kg de presunto e 0.50Kg de carne." |
+| 10         | 0.50                        | 0.50                         | 1.00                        | "Será necessário comprar 0.50Kg de queijo, 0.50Kg de presunto e 1.00Kg de carne." |
+| 20         | 1.00                        | 1.00                         | 2.00                        | "Será necessário comprar 1.00Kg de queijo, 1.00Kg de presunto e 2.00Kg de carne." |
+ */
+
+import javax.swing.JOptionPane;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        double queijo,presunto,carne,sanduba;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //Strings
+        String ssanduba;
+
+        ssanduba = JOptionPane.showInputDialog(null,"Quantos sanduíches serão feitos?");
+        sanduba = Double.parseDouble(ssanduba);
+
+        //Main code
+        queijo = (sanduba * 50)/1000;
+        presunto = (sanduba * 50)/1000;
+        carne = (sanduba * 100)/1000;
+
+        //Output
+        JOptionPane.showMessageDialog(null,"Será necessário comprar " + queijo + "Kg de queijo, " + presunto + "Kg de presunto e " + carne + "Kg de carne.");
     }
+}
 }
